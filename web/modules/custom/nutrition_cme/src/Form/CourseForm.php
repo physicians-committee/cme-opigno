85285ca8225f3cd8a75b3224f29d4fd427aad40a
Karen Horrocks <khorrocks@pcrm.org>
2020-10-16 16:13:18 -0400

    Add nutrition_cme module

---
+++ web/modules/custom/nutrition_cme/src/Form/CourseForm.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\nutrition_cme\Form\CourseForm.
+ */
+namespace Drupal\nutrition_cme\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class CourseForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'course_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['course_name'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Course Name:'),
+      '#required' => TRUE,
+    );
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+      '#button_type' => 'primary',
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (!preg_match('/^[a-zA-Z0-9 ]+$/i', $form_state->getValue('course_name'))) {
+      $form_state->setErrorByName('course_name', $this->t('Course name must be alphanumeric.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    foreach ($form_state->getValues() as $key => $value) {
+      drupal_set_message($key . ': ' . $value);
+    }
+  }
+}
