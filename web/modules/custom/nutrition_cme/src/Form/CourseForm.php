469520334732a2c7c91646758aecaa0dd6f702b8
Karen Horrocks <khorrocks@pcrm.org>
2020-10-16 20:32:30 -0400

    Update to create learning_path group.

--- web/modules/custom/nutrition_cme/src/Form/CourseForm.php
+++ web/modules/custom/nutrition_cme/src/Form/CourseForm.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\nutrition_cme\Form;
 
-use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -51,7 +50,12 @@ class CourseForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $course = Group::create(['type' => 'learning_path', 'title' => $form_state->getValue('course_name')]);
+    $course = Group::create(
+      [
+        'type' => 'learning_path', 
+        'title' => $form_state->getValue('course_name')
+      ]
+    );
     $course->save();
     foreach ($form_state->getValues() as $key => $value) {
       drupal_set_message($key . ': ' . $value);
